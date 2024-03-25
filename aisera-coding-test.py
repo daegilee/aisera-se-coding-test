79554dae68e6bcf696683ff030fccdba34b8fb9d
daegilee <daegi7777@gmail.com>
2024-03-25 13:24:06 -0500

    code change
    changed to write in readable format

--- aisera-coding-test.py
+++ aisera-coding-test.py
@@ -6,17 +6,22 @@ import json
 url = "https://exchange-rate-api1.p.rapidapi.com/convert"
 
 # change base and target values with corresponding currency codes 
-querystring = {"base":"USD","target":"KRW"}
+querystring = {"base":"USD","target":"MXN"}
 
 # insert user's API key value to test 
 headers = {
-	"X-RapidAPI-Key": "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"X-RapidAPI-Key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 	"X-RapidAPI-Host": "exchange-rate-api1.p.rapidapi.com"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
 
-# take JSON response and write to text file every time request is made
-with open("conversion.txt", "a") as f:
-    json.dump(response.json(), f, indent=2)
+# capture values for rate, base, and target in json response
+rate = str(response.json()["convert_result"]["rate"])
+base = response.json()["convert_result"]["base"]
+target = response.json()["convert_result"]["target"]
+
+# write conversion rate in readable format
+f = open("conversion.txt", "a")
+f.write("The conversion rate for " + base + " to " + target + " is " + rate + ".\n\n")    
 f.close()
