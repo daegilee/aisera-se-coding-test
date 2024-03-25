cfaf0569f3e61e0080dd1e22fa2898d313f4375a
daegilee <daegi7777@gmail.com>
2024-03-25 13:48:31 -0500

    code change

--- aisera-coding-test.py
+++ aisera-coding-test.py
@@ -10,16 +10,16 @@ querystring = {"base":"USD","target":"MXN"}
 
 # insert user's API key value to test 
 headers = {
-	"X-RapidAPI-Key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"X-RapidAPI-Key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 	"X-RapidAPI-Host": "exchange-rate-api1.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+response = requests.get(url, headers=headers, params=querystring).json()
 
 # capture values for rate, base, and target in json response
-rate = str(response.json()["convert_result"]["rate"])
-base = response.json()["convert_result"]["base"]
-target = response.json()["convert_result"]["target"]
+rate = str(response["convert_result"]["rate"])
+base = response["convert_result"]["base"]
+target = response["convert_result"]["target"]
 
 # write conversion rate in readable format
 f = open("conversion.txt", "a")
